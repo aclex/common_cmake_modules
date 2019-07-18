69c85b782be2d655489aa5fffacc1f05500259be
Alexey Chernov <4ernov@gmail.com>
2019-07-18 15:17:06 +0300

    Fixes in `AddUninstallTarget` module
    1. Don't add duplicated uninstall target, if it's already exists
    (e.g. if the current project is included as subproject via
    `add_subdirectory()` from parent CMake project), as it leads to
    CMake error.
    
    2. Make the code a function, not a macro, to better preserve
    variables locality.
    
    3. Call the function right away, so that inclusion of the module
    adds the `uninstall` target automatically.

--- Modules/AddUninstallTarget.cmake
+++ Modules/AddUninstallTarget.cmake
@@ -1,11 +1,15 @@
 set(THIS_IS_MY_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-macro(add_uninstall_target)
-	set(UNINSTALL_CMAKE_FILENAME "cmake_uninstall.cmake")
-	configure_file(
-		"${THIS_IS_MY_DIR}/${UNINSTALL_CMAKE_FILENAME}.in"
-		"${CMAKE_BINARY_DIR}/${UNINSTALL_CMAKE_FILENAME}"
-		IMMEDIATE @ONLY)
+function(add_uninstall_target)
+	if (NOT TARGET uninstall)
+		set(UNINSTALL_CMAKE_FILENAME "cmake_uninstall.cmake")
+		configure_file(
+			"${THIS_IS_MY_DIR}/${UNINSTALL_CMAKE_FILENAME}.in"
+			"${CMAKE_BINARY_DIR}/${UNINSTALL_CMAKE_FILENAME}"
+			IMMEDIATE @ONLY)
 
-	add_custom_target(uninstall COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/${UNINSTALL_CMAKE_FILENAME})
-endmacro(add_uninstall_target)
+		add_custom_target(uninstall COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/${UNINSTALL_CMAKE_FILENAME})
+	endif ()
+endfunction(add_uninstall_target)
+
+add_uninstall_target()
