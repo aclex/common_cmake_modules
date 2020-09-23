3eded9e700339055f7f14d0a2eca5f30ac74081e
Alexey Chernov <4ernov@gmail.com>
2020-09-23 22:47:17 +0300

    GitVersion: add function to directly set project version

--- Modules/GitVersion.cmake
+++ Modules/GitVersion.cmake
@@ -6,6 +6,8 @@
 # Writes current project version into variable 'VER'
 #
 
+include(VersionRoutines)
+
 function(git_version VER)
 	execute_process(COMMAND git describe --tags
 					COMMAND sed s/^v//
@@ -31,5 +33,30 @@ function(git_commit_version VER)
 		set(${VER} ${TEMP_VER} PARENT_SCOPE)
 	else()
 		set(${VER} "0.0.0" PARENT_SCOPE)
+	endif()
+endfunction()
+
+function(set_project_version_from_git_tag)
+	find_program(GIT_CMD git)
+	find_program(SED_CMD sed)
+
+	if (NOT GIT_CMD OR NOT SED_CMD)
+		return()
+	endif()
+
+	execute_process(COMMAND git describe --tags
+					COMMAND sed s/^v//
+					OUTPUT_VARIABLE TEMP_VER
+					ERROR_QUIET)
+	if(TEMP_VER)
+		string(REGEX REPLACE "(\r?\n)+$" "" TEMP_VER "${TEMP_VER}")
+		string(REGEX REPLACE "\-.*$" "" TEMP_VER "${TEMP_VER}")
+		set(PROJECT_VERSION ${TEMP_VER} PARENT_SCOPE)
+		split_version_string(${TEMP_VER} PROJECT_VERSION)
+
+		if (NOT CMAKE_PROJECT_VERSION)
+			set(CMAKE_PROJECT_VERSION ${TEMP_VER} PARENT_SCOPE)
+			split_version_string(${TEMP_VER} CMAKE_PROJECT_VERSION)
+		endif()
 	endif()
 endfunction()
